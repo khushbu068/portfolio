26b1ef26e113b15ce1a7dfecd88fa9728d8a39f8
Khushbu chacholiya <chacholiyakhushbu068@gmail.com>
2025-01-27 00:16:48 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
             </p>
 
             <div className='my-8'>
-                <a href='my_resume.pdf' className="px-6 py-3 w-full rounded-xl mr-4 
+                <a href='my_resume1.pdf' className="px-6 py-3 w-full rounded-xl mr-4 
                 bg-gradient-to-br from-orange-500 to-pink-500 text-white">
                  Download CV
                 </a>
